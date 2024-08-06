3493cd60b48d20616f688b02a82ba8c2bee9ba13
telecomadm1145 <telecomadm1919@gmail.com>
2024-08-06 16:04:11 +0800

    a

--- test.cpp
+++ test.cpp
@@ -4,13 +4,17 @@ extern "C" void render_copy();
 
 int main()
 {
-	char test[30] = "Count:";
 	reset_sfrs();
-	Timer0Interval = 65535;
-	ushort n = itoa_n(atoi_n<long>( (char __near *)"07210721"), (char __near *)(test + 6));
-	*(test + n + 6) = '\n';
-	*(test + n + 6 + 1) = 0;
-	DebugOutputString(test);
 	while (1)
-		delay(1000);
+	{
+		auto kv = wait_kiko();
+		DebugOutputString("Got key!\n");
+		char buf[4];
+		itoa_n(kv.ki, (char __near *)buf);
+		DebugOutputString(buf);
+		DebugOutputString(",");
+		itoa_n(kv.ko, (char __near *)buf);
+		DebugOutputString(buf);
+		DebugOutputString("\n");
+	}
 }
